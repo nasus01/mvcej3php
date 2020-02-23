50d05c40e75988a17b4119610346475d693ba6be
cristian morales loaiza <christianjack01@hotmail.com>
2020-02-23 13:34:45 -0500

    tercer ejemplo de mvc php con mysql

---
+++ model/Tblcategorias_model.php
@@ -0,0 +1,37 @@
+<?php
+
+
+    class Tblcategorias_model{
+       private $bd;
+       private $tblcategorias;
+     
+        public function __construct(){
+
+         $this->bd=conexion::getConexion();
+         $this->tblcategorias = array();
+
+        }
+        public function insertarCategoria($dato){
+            $nombre = $dato ['nombre'];
+            $consulta = "INSERT INTO tblcategorias (nombre) VALUES ('$nombre')";
+            mysqli_query($this->bd,$consulta) or die ("error al insertar la categoria");
+        }
+
+        public function consultarCategorias(){
+            $consulta=$this->bd->query("SELECT * FROM tblcategorias");
+            while($fila=$consulta->fetch_assoc()){
+                $this->tblcategorias[]=$fila;
+            }
+            return $this->tblcategorias;
+
+
+
+        }
+
+
+
+
+    }
+
+
+?>
---
+++ view/index.php
@@ -0,0 +1,7 @@
+      <?php require_once 'header.php'; ?>
+        <h1>Menú principal</h1>
+        <br>
+        <a href="./?accion=menuCategorias">Categorias producto</a>
+        <br>
+        <a href="./?accion=menuProductos">Productos</a>
+        <?php require_once 'footer.php'; ?>
